94f702b0b3ecd3fb9180991631e460d24c630a1d
alon gvili <asingvili@gmail.com>
2022-02-26 02:14:38 +0200

    start to put some info

--- app/routes/lists.tsx
+++ app/routes/lists.tsx
@@ -1,7 +1,10 @@
 export default function Projects() {
     return (
         <div>
-            <h1 className="text-2xl text-[#394359]">Projects</h1>
+            <h1 className="text-6xl max-w-2xl text-[#394359] uppercase font-bold">
+				My <span className="text-[#A266CA]">List </span>
+				 of things that <span className="text-[#CBADCC]">I think are  awesome</span>
+			</h1>
         </div>
     );
 }
--- app/routes/me.tsx
+++ app/routes/me.tsx
@@ -1,7 +1,10 @@
 export default function Projects() {
     return (
         <div>
-            <h1 className="text-2xl text-[#394359]">Projects</h1>
+             <h1 className="text-6xl max-w-2xl text-[#394359] uppercase font-bold">
+				Hey i'm <span className="text-[#A266CA]">Alon Gvili </span>
+				 38 years old <span className="text-[#CBADCC]">web developer </span>
+			</h1>
         </div>
     );
 }
